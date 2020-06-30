36d344063c006ccbd58d37be51d79a8b3ac91400
Pedro Rio <pedrocacaisrio@gmail.com>
2020-06-30 16:25:48 +0100

    Rehash anyenvs and install yarn

--- scripts/languages.sh
+++ scripts/languages.sh
@@ -4,8 +4,12 @@ anyenv install pyenv
 pyenv install 3.7.0
 pyenv local 3.7.0
 pyenv global 3.7.0
+pyenv rehash
 
 anyenv install nodenv
-nodenv install 3.7.0
-nodenv local 3.7.0
-nodenv global 3.7.0
+nodenv install 14.4.0
+nodenv local 14.4.0
+nodenv global 14.4.0
+nodenv rehash
+
+curl -o- -L https://yarnpkg.com/install.sh | bash
